b6b527f987281b5624595d7739a7d19864d2e304
Balaji Punnuru <balaji_punnuru@cable.comcast.com>
2015-11-20 15:51:47 -0800

    ARRISXB3-3660: Added back dbus startup from cosa_start.
    Reason for change: As all the ccsp processes need the dbus
                       to be connected to local vlan. That particular
                       local vlan is brought up in this script,
                       hence it is added back here.
    
    Test Procedure: Build ad Verify all the processes are talking on dbus.
    
    Risks: Low.
    
    Change-Id: I1350bb449fd068f8b9ffef8b4ba1d12a2f442a59
    Signed-off-by: Balaji Punnuru <balaji_punnuru@cable.comcast.com>

--- scripts/cosa_start_arm.sh
+++ scripts/cosa_start_arm.sh
@@ -119,8 +119,7 @@ cp ccsp_msg.cfg /tmp
 
 # have IP address for dbus config generated
 #./DbusCfg
-#We are starting dbus from systemd service
-#$BINPATH/dbus-daemon --config-file=./basic.conf --fork
+$BINPATH/dbus-daemon --config-file=./basic.conf --fork
 
 mkdir -p /var/tmp/logs/
 
