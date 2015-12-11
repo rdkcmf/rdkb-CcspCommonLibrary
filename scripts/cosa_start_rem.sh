f6b7fd8408e2de6971b5436dc36bb4d2d2f66164
Balaji Punnuru <balaji_punnuru@cable.comcast.com>
2015-12-11 12:55:18 -0500

    ARRISXB3-3845: Modified cosa_start_rem.sh to invoke webpa.
    Reason for change: Added webpa invocation.
    
    Test Procedure: Build and Verify.
    
    Risks: None.
    
    Change-Id: Id642bb7ab51e2958de4efe7b9922fd559613016e
    Signed-off-by: Balaji Punnuru <balaji_punnuru@cable.comcast.com>

--- scripts/cosa_start_rem.sh
+++ scripts/cosa_start_rem.sh
@@ -131,51 +131,35 @@ if [ -e ./tad ]; then
         cd ..
 fi
 
-#if [ -e ./webpa ]; then
-#	cd webpa
-#	
-#	if [ "x"$Subsys = "x" ];then
-#		./webpa
-#	else
-#		echo "./webpa -subsys $Subsys"
-#		./webpa -subsys $Subsys
-#	fi
-#	cd ..
-#fi
-
-if [ -e ./webpa ]; then
         
-	if [ -e /nvram/webpa_cfg.json ]; then
-		echo "webpa_cfg.json exists in nvram"
-	 else	
-		echo "webpa_cfg.json not found in nvram"
-		cp /usr/ccsp/webpa/webpa_cfg.json /nvram/webpa_cfg.json
-		echo "webpa_cfg.json file does not exist. Hence copying the factory default file.."
-	fi
+if [ -e /nvram/webpa_cfg.json ]; then
+	echo "webpa_cfg.json exists in nvram"
+ else
+	echo "webpa_cfg.json not found in nvram"
+	cp /etc/webpa_cfg.json /nvram/webpa_cfg.json
+	echo "webpa_cfg.json file does not exist. Hence copying the factory default file.."
+fi
         
-	WEBPAVER=`cat /nvram/webpa_cfg.json | grep "file-version" | awk '{print $2}' | sed 's|[\"\",]||g'`
-	echo "WEBPAVER is $WEBPAVER"
-	if [ "$WEBPAVER" = "" ];then	        
-		cp /usr/ccsp/webpa/webpa_cfg.json /nvram/webpa_cfg.json
-		echo "Copying factory default file as webpa file-version does not exist in current cfg file.."
-	fi
+WEBPAVER=`cat /nvram/webpa_cfg.json | grep "file-version" | awk '{print $2}' | sed 's|[\"\",]||g'`
+echo "WEBPAVER is $WEBPAVER"
+if [ "$WEBPAVER" = "" ];then
+	cp /etc/webpa_cfg.json /nvram/webpa_cfg.json
+	echo "Copying factory default file as webpa file-version does not exist in current cfg file.."
+fi
 	
-	ENABLEWEBPA=`cat /nvram/webpa_cfg.json | grep EnablePa | awk '{print $2}' | sed 's|[\"\",]||g'`
-	echo "ENABLEWEBPA is $ENABLEWEBPA"
+ENABLEWEBPA=`cat /nvram/webpa_cfg.json | grep EnablePa | awk '{print $2}' | sed 's|[\"\",]||g'`
+echo "ENABLEWEBPA is $ENABLEWEBPA"
 
-	if [ "$ENABLEWEBPA" = "true" ];then
-		echo "ENABLEWEBPA is true..Intializing WebPA.."
-		cd webpa
-		if [ "x"$Subsys = "x" ];then
-			$BINPATH/webpa
-		else
-			echo "./webpa -subsys $Subsys"
-			$BINPATH/webpa -subsys $Subsys
-		fi
-		cd ..
+if [ "$ENABLEWEBPA" = "true" ];then
+	echo "ENABLEWEBPA is true..Intializing WebPA.."
+	if [ "x"$Subsys = "x" ];then
+		$BINPATH/webpa
 	else
-		echo "EnablePa parameter is set to false. Hence not initializng WebPA.."
+		echo "./webpa -subsys $Subsys"
+		$BINPATH/webpa -subsys $Subsys
 	fi
+else
+	echo "EnablePa parameter is set to false. Hence not initializng WebPA.."
 fi
 
 if [ -e ./ccspRecoveryManager ]; then
