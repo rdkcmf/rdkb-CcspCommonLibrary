3f85147b3aa9ca5c37bde4caebfbebd10baa7578
tconnors <thomas_connors@cable.comcast.com>
2016-07-28 19:22:55 +0000

    XF3-566 : Fixes LAN Ethernet Status
    Reason for change: Provide Accurate Ethernet Link Status
    Test Procedure: Connect a laptop and navigate to Hardware->LAN Ethernet page
    Risks: This change creates a problem in that now all Connection Speeds are displayed as 1000 Mbps even when not conneted
    
    Change-Id: I46dd858fbf6946ee895abeba3a96b3672a0bf025
    Signed-off-by: tconnors <thomas_connors@cable.comcast.com>

--- scripts/cosa_start_mips.sh
+++ scripts/cosa_start_mips.sh
@@ -63,6 +63,7 @@ fi
 export LD_LIBRARY_PATH=$PWD:.:$PWD/../../lib:$PWD/../../.:/lib:/usr/lib:$LD_LIBRARY_PATH
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/var/run/dbus/system_bus_socket
 export LOG4C_RCPATH=/etc
+export lattice_cfg=/etc/lattice-config.xml
 
 # Check if bbhm has Notify flag present
 NOTIFYPRESENT=`cat /nvram/bbhm_cur_cfg.xml | grep NotifyWiFiChanges`
