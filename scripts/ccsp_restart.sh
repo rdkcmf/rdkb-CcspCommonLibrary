1dfb8289c0a759ecefac37944f11e0b419d7cbcd
Santosh Nayak <Santosh_Nayak03@infosys.com>
2016-11-10 00:49:06 +0000

    RDKB-8912 : Ccsp PandM Deadlock cases
    Reason for change: There is not enough between process initialization after atom reset ,PandM is going to deadlock condition.
    Test Procedure: Verified on target.
    Risks: None
    Signed-off-by: Santosh Nayak Santosh_Nayak03@infosys.com
    
    Change-Id: Ie7662fb6625da0503228896bac5a179b2df29c24

--- scripts/ccsp_restart.sh
+++ scripts/ccsp_restart.sh
@@ -9,19 +9,63 @@ exec 3>&1 4>&2 >>$SELFHEALFILE 2>&1
 	killall PsmSsp
         killall self_heal_connectivity_test.sh
         killall resource_monitor.sh
+
+	if [ -f "/tmp/logagent_initialized" ]
+	then
+		rm -rf /tmp/logagent_initialized
+	fi
         killall log_agent
+
+	if [ -f "/tmp/pam_initialized" ]
+	then
+		rm -rf /tmp/pam_initialized
+	fi
         killall CcspPandMSsp
+
+	if [ -f "/tmp/wecb_initialized" ]
+	then
+		rm -rf /tmp/wecb_initialized
+	fi
         killall CcspWecbController
+
+	if [ -f "/tmp/Notify_initialized" ]
+	then
+		rm -rf /tmp/Notify_initialized
+	fi
         killall notify_comp
         killall CcspHomeSecurity
+
+	if [ -f "/tmp/cm_initialized" ]
+	then
+		rm -rf /tmp/cm_initialized
+	fi
         killall CcspCMAgentSsp
+
+	if [ -f "/tmp/moca_initialized" ]
+	then
+		rm -rf /tmp/moca_initialized
+	fi
 	killall CcspMoCA
         killall CcspTr069PaSsp
         killall CcspTandDSsp
+
+	if [ -f "/tmp/webpa_initialized" ]
+	then
+		rm -rf /tmp/webpa_initialized
+	fi
         killall webpa
         killall CcspSafeNAT
+
+	if [ -f "/tmp/lmlite_initialized" ]
+	then
+		rm -rf /tmp/lmlite_initialized
+	fi
         killall CcspLMLite
         killall CcspXdnsSsp
+	if [ -f "/tmp/mta_initialized" ]
+	then
+		rm -rf /tmp/mta_initialized
+	fi
         killall CcspMtaAgentSsp
         killall CcspHotspot
 
@@ -54,16 +98,6 @@ ulimit -c unlimited
 
         sleep 20
 
-	echo "RDKB_SELFHEAL : Resetting process log_agent on atom reset"
-
-        cd /usr/ccsp/logagent
-
-        if [ "x"$Subsys = "x" ];then
-                $BINPATH/log_agent
-        else
-                echo "$BINPATH/log_agent -subsys $Subsys"
-                $BINPATH/log_agent -subsys $Subsys
-        fi
 	
 	#notify-comp
         echo "RDKB_SELFHEAL : Resetting process notify-comp on atom reset"
@@ -84,6 +118,29 @@ ulimit -c unlimited
 		 $BINPATH/CcspPandMSsp -subsys $Subsys
 	fi
 	# We need to check whether to enable captive portal flag
+
+        sleep 15
+	count=0
+	MAX_COUNT=20
+	while :
+	do
+	if [ -f "/tmp/pam_initialized" ]
+	then
+		echo "PandM initialization complete after atom reset, initializing other CCSP processes"
+		break
+	else
+		echo "Waiting for PandM initialization to complete"
+		count=$((count+1))
+		sleep 10
+		if [ "$count" -ge "$MAX_COUNT" ]
+		then
+			echo "PandM initialization reaches MAX time out, breaking the loop"
+			break
+		fi	
+		
+
+	fi
+	done
 	checkCaptivePortal
         sleep 3
        
@@ -96,6 +153,7 @@ ulimit -c unlimited
              $BINPATH/CcspCMAgentSsp -subsys $Subsys
         fi
 
+	sleep 3
 	echo "RDKB_SELFHEAL : Resetting process CcspMoCA on atom reset"
 
 	cd /usr/ccsp/moca
@@ -105,7 +163,7 @@ ulimit -c unlimited
 	$BINPATH/CcspMoCA -subsys $Subsys
 	fi
 
-	
+	sleep 3
 	#mta
 	echo "[`getDateTime`] RDKB_SELFHEAL : Resetting process CcspMtaAgentSsp on atom reset"
 	cd /usr/ccsp/mta
@@ -125,7 +183,7 @@ ulimit -c unlimited
         $BINPATH/webpa -subsys $Subsys
     	fi
 
-
+	sleep 3
 	 #td
 	echo "[`getDateTime`] RDKB_SELFHEAL : Resetting process CcspTandDSsp on atom reset"
 	cd /usr/ccsp/tad
@@ -135,6 +193,7 @@ ulimit -c unlimited
                 $BINPATH/CcspTandDSsp -subsys $Subsys
         fi
 
+	sleep 3
 	 #safenet
 	echo "[`getDateTime`] RDKB_SELFHEAL : Resetting process CcspSafeNAT on atom reset"
 	cd /usr/ccsp/ccsp-safenat-broadband
@@ -144,7 +203,7 @@ ulimit -c unlimited
                 $BINPATH/CcspSafeNAT -subsys $Subsys
         fi
 
-
+	sleep 3
 	#CcspWiecbController
         echo "RDKB_SELFHEAL : Resetting process CcspWecbController on atom reset"
 	cd /usr/ccsp/wecb
@@ -154,16 +213,19 @@ ulimit -c unlimited
        		$BINPATH/CcspWecbController -subsys $Subsys
     	fi
 
+	sleep 3
 	 #CcspLMLite
 	echo "[`getDateTime`] RDKB_SELFHEAL : Resetting process CcspLMLite on atom reset"
 	cd /usr/ccsp/lm
 	$BINPATH/CcspLMLite -subsys $Subsys
 
-     #CcspXdnsSsp
+	sleep 2
+
+ #CcspXdnsSsp
     echo "[`getDateTime`] RDKB_SELFHEAL : Resetting process CcspXdnsSsp on atom reset"
     cd /usr/ccsp/xdns
     $BINPATH/CcspXdnsSsp -subsys $Subsys
-	
+
 	xfinityenable=`psmcli get dmsb.hotspot.enable`
 
 	if [ $xfinityenable -eq 1 ];then
@@ -174,6 +236,7 @@ ulimit -c unlimited
 	cd -
 	fi
 
+	sleep 2
 	#CcspHomeSecurity
 	echo "[`getDateTime`] RDKB_SELFHEAL : Resetting process CcspHomeSecurity on atom reset"
 	CcspHomeSecurity 8081&
@@ -187,4 +250,15 @@ ulimit -c unlimited
         else
 		$BINPATH/CcspTr069PaSsp -subsys $Subsys
         fi
-	
+
+	sleep 2
+	echo "RDKB_SELFHEAL : Resetting process log_agent on atom reset"
+
+        cd /usr/ccsp/logagent
+
+        if [ "x"$Subsys = "x" ];then
+                $BINPATH/log_agent
+        else
+                echo "$BINPATH/log_agent -subsys $Subsys"
+                $BINPATH/log_agent -subsys $Subsys
+        fi
