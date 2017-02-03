cf9308a106dbc1160a115464412ab5d92ff99413
Amit Ghorpade <amit_ghorpade@comcast.com>
2017-02-03 10:33:47 -0800

    RDKB-7497: Approval Cisco Proprietary Copyright.
    Reason for change: This patch is to remove the cisco proprietary
                       copyrights.
    
    Test Procedure: Compile Ccsp Common Lib, Build image & power on XB3.
    
    Risks: None
    
    Change-Id: I5da94a6fc57cd0f77c33d2740bd6b64c9547485a
    Signed-off-by: Amit_Ghorpade@cable.comcast.com

--- source/ccsp/include/ccsp_base_api.h
+++ source/ccsp/include/ccsp_base_api.h
@@ -40,20 +40,6 @@
 
     ---------------------------------------------------------------
 
-                    Copyright (c) 2011, Cisco Systems, Inc.
-
-                            CISCO CONFIDENTIAL
-              Unauthorized distribution or copying is prohibited
-                            All rights reserved
-
- No part of this computer software may be reprinted, reproduced or utilized
- in any form or by any electronic, mechanical, or other means, now known or
- hereafter invented, including photocopying and recording, or using any
- information storage and retrieval system, without permission in writing
- from Cisco Systems, Inc.
-
-    ---------------------------------------------------------------
-
     environment:
 
         platform dependent
