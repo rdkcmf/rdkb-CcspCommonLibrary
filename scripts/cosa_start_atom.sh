efb085129664df55cab2bc9e56b6a0791ef06bd3
Amit <amit_ghorpade@comcast.com>
2017-06-08 18:45:47 +0000

    ARRISXB3-6159: Need to enable SSID 4 (XH 5G) by default.
    Reason for change: This change will Enable SSID 4 for XH
    5GHZ by default.
    
    Test Procedure: Power on the XB3.
    On ATOM , verify iwconfig command.
    ath3 should be up in iwconfig.
    
    Risks: None
    
    Change-Id: I8fcb8b9e7f7e2c863c57ca6922df82df3bdfc11e
    Signed-off-by: Amit_Ghorpade@cable.comcast.com

--- scripts/cosa_start_atom.sh
+++ scripts/cosa_start_atom.sh
@@ -98,6 +98,10 @@ fi
 # This script is only for ARRISXB3 and Ciscoxb3. All other product do not need this step.
 rdk_wifi_upgrade_vlan106
 
+# Enable XH for 5GHZ by default.
+# This script is only for ARRISXB3 and Ciscoxb3.
+rdk_wifi_upgrade_xh5
+
 # Config vlan 106 Secure ssid (phrase 2): RDKB-6974
 /lib/rdk/rdk_wifi_configure_lnf_ap.sh
 
