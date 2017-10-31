1b8c6f2dc31e1f529259d54ba33d99f3f7eb1787
Ramki Ananthakrishnan <ramki_ananthakrishnan@cable.comcast.com>
2017-10-31 05:16:12 +0000

    RDKB-14632: Deploy aker on Yocto.
    Reason for change: Support for aker, parental control component on Yocto.
    Test Procedure: Build and verify.
    Risks: None.
    Signed-off-by: Ramki Ananthakrishnan <ramki_ananthakrishnan@cable.comcast.com>
    
    Change-Id: Ieb1d7bc1672bd480cfbe37e4f837821873288a18

--- scripts/cosa_start_rem.sh
+++ scripts/cosa_start_rem.sh
@@ -26,6 +26,16 @@ else
 fi
 
 
+if [ -f "/etc/PARODUS_ENABLE" ]; then
+    echo "/usr/bin/aker -p $PARODUS_URL -c $AKER_URL -w echo -d /nvram/pcs.bin -m /nvram/pcs.bin.md5" > /tmp/aker_cmd.cmd
+    aker_cmd=`cat /tmp/aker_cmd.cmd`
+    $aker_cmd &
+    echo_t "aker process should have been started"
+else
+    echo_t "aker NOT enabled"
+fi
+
+
 if [ "x$ENABLE_SNMPv3" == "xtrue" ]; then
     if [ -f /lib/rdk/run_snmpv3_master.sh ]; then
         /lib/rdk/run_snmpv3_master.sh &
