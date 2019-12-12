796e128fa64abafce3c90cecd55d17ff6ff8fac4
anuchacko01 <achacko.contractor@libertyglobal.com>
2019-12-12 12:01:09 +0530

    RDKBDEV-83 : Device Gateway Association using Manageable Device
    Reason For Change:
    	   : Added changes to include active notification for the ManageableDeviceNumberOfEntries parameter.
    
    Test Procedure:
        Detailed Test plan and Report has been attached into the JIRA ticket.
    
    Change-Id: I1355cc48980dbbb976cd1e01f168e97a328c8585
    Risks: None
    Signed-off-by: Anu Chacko <achacko.contractor@libertyglobal.com>

--- source/ccsp/components/common/DataModel/dml/components/DslhVarRecord/dslh_varro_states.c
+++ source/ccsp/components/common/DataModel/dml/components/DslhVarRecord/dslh_varro_states.c
@@ -510,9 +510,12 @@ DslhVarroSetNotification
 	pFullName = pMyObject->GetFullName(pMyObject);
 	if(strstr(pFullName,"NumberOfEntries"))
 	{
-		AnscTraceEntry(("Param '%s' cannot be set to Notification Active.\n", pFullName));
-                AnscFreeMemory(pFullName);
-		return  ANSC_STATUS_UNAPPLICABLE;
+	    if(!strstr(pFullName,"ManageableDeviceNumberOfEntries"))
+        {
+            AnscTraceEntry(("Param '%s' cannot be set to Notification Active.\n", pFullName));
+            AnscFreeMemory(pFullName);
+            return  ANSC_STATUS_UNAPPLICABLE;
+        }
 	}
         AnscFreeMemory(pFullName);
     if ( iNotification == DSLH_CWMP_NOTIFICATION_active && pVarEntity->ParamDescr->NotifyStatus != DSLH_CWMP_NOTIFY_STATUS_configActive)
