e40dffb09a1ace3070f6edfc590d0828865f91f7
Harikrishnan Raman <Harikrishnan_Raman@comcast.com>
2019-12-18 09:19:25 +0000

    ARRISXB3-9724 : SSID - Xfi - iwconfig - [ RBUS ]
    Reason for change:
    in rbus mode, the component name is returned as dummy for individual
    queries and we return the same parameter for wildcard queries.
    Here apply settings isn't called from webpa as it is matching the
    comp name "Device.WiFi" with "eRT.com.cisco.spvtg.ccsp.wifi"
    now we have modified the code and it will return first two
    root nodes and not dummy
    
    Test Procedure:
    change ssid from xfi and check if it is getting
    modified in iwconfig and is broadcasting with new name
    Risks: Medium
    Signed-off-by:Harikrishnan_Raman@comcast.com
    
    (cherry picked from commit 7a144e9b020e25c35429538278d5fa679c5a6361)
    
    Change-Id: I633131cc3aa7768704189384f62c420bbcdcca14

--- source/ccsp/include/ccsp_base_api.h
+++ source/ccsp/include/ccsp_base_api.h
@@ -1276,4 +1276,5 @@ void PsmFreeRecords(void *bus_handle,
         int nrec);
 
 int Rbus_to_CCSP_error_mapper(int error_code);
+int obj_name_finder(char *paramName, char **compName, int *pfree);
 #endif /* CCSP_BASE_API_H */
