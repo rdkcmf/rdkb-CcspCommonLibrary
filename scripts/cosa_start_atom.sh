192cf01e71b9ab4b07edbd7c3fd873e30c6bf183
tganes735 <tamilarasi_ganesan@comcast.com>
2020-05-19 05:37:42 +0000

    RDKB-29373 : vapup percentage is calculating in wifi_monitor.c
    Reason for change: vapup percentage is calculating in wifi_monitor.c so removing this script to avoid the calculation again
    Test Procedure: Only for test purpose
    Risks: Low
    Signed-off-by:tamilarasi_ganesan@comcast.com
    
    (cherry picked from commit 818def037db453de8c25e729af02601d134bc09e)
    Change-Id: I562c69023fe36d67f8ba1d5efc130e920817887c

--- scripts/cosa_start_atom.sh
+++ scripts/cosa_start_atom.sh
@@ -278,9 +278,6 @@ if [ "$MFG_NAME" = "Cisco" ]; then
     /usr/ccsp/wifi/wifi_default_radius_port.sh
 fi
 
-echo_t "starting apshealth.sh"
-/usr/ccsp/wifi/apshealth.sh &
-
 echo_t "starting stahealth_log.sh"
 sh /usr/ccsp/wifi/stahealth_log.sh &
 
