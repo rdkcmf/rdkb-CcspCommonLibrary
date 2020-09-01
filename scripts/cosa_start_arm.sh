ae32de40e525c64218cb4edaba66513410335fd2
plaksh175 <PiramanayagamMoneka_Lakshmi@comcast.com>
2020-09-01 10:40:45 +0000

    RDKB-29893 : To mount tmp directory with options
    Reason for change: Moving the scripts from /etc/dibbler to /lib/rdk/ path
    Test Procedure: Build and Verify
    Risks: None
    
    Signed-off-by: plaksh175 <PiramanayagamMoneka_Lakshmi@comcast.com>
    (cherry picked from commit f690af4f70e327382bb8b5fb9a6e0b83f999b077)
    (cherry picked from commit e6039deeb92ca9a49617a01d6b5a7772fb46c700)
    Change-Id: Ic7cc88775b5ed72603ba9e2f4a2741618fa794e9

--- scripts/cosa_start_arm.sh
+++ scripts/cosa_start_arm.sh
@@ -264,5 +264,5 @@ fi
 #running dibbler init script 
 if [ $MODEL_NUM == "DPC3941B" ] || [ $MODEL_NUM == "DPC3939B" ] || [ "$MODEL_NUM" = "CGA4131COM" ]; then
 echo "Coping server-notify.sh into /var/lib/dibbler"
-sh /etc/dibbler/dibbler-server-init.sh
+sh /lib/rdk/dibbler-server-init.sh
 fi
