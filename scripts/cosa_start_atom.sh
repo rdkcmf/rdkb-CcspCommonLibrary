a8b08c16a4099f3119d2d6a546cffda4f5c6b155
Mohan Singh <Mohan_Singh@comcast.com>
2021-03-25 23:07:54 +0000

    Revert "RDKB-34044 : Remove radio health scripts"
    Reason for revert: Due to regression RDKB-34874
    
    This reverts commit f251aa31c8755e45d4c6241261f652b4dae0c047.
    
    (cherry picked from commit 2a798a5f6f735b28b84a24dd3e666ae91e7ff1bc)
    Change-Id: I4f077709b1729e23b32b04b549f2c3208b23f4a3

--- scripts/cosa_start_atom.sh
+++ scripts/cosa_start_atom.sh
@@ -290,6 +290,7 @@ then
 			echo "35 * * * *  /usr/ccsp/wifi/aphealth_log.sh" >> $CRONFILE
 			echo "47 2 * * *  /usr/ccsp/wifi/dailystats_log.sh" >> $CRONFILE
 			echo "30 * * * * /rdklogger/log_cpu_info_atom.sh"  >> $CRONFILE
+			echo "*/15 * * * * sh /usr/ccsp/wifi/radiohealth.sh" >> $CRONFILE
 			echo "1 */12 * * *  /usr/ccsp/wifi/mesh_status.sh" >> $CRONFILE
 		else
 			if [ ! -d $CRONPATH ]
@@ -302,6 +303,7 @@ then
 			echo "35 * * * *  /usr/ccsp/wifi/aphealth_log.sh" >> $CRONFILE
 			echo "47 2 * * *  /usr/ccsp/wifi/dailystats_log.sh" >> $CRONFILE
 			echo "30 * * * * /rdklogger/log_cpu_info_atom.sh"  >> $CRONFILE
+			echo "*/15 * * * * sh /usr/ccsp/wifi/radiohealth.sh" >> $CRONFILE
 			echo "1 */12 * * *  /usr/ccsp/wifi/mesh_status.sh" >> $CRONFILE
 		fi
 		crond -c $CRONPATH -l 9
@@ -313,6 +315,7 @@ then
 		echo "35 * * * *  /usr/ccsp/wifi/aphealth_log.sh" >> $CRONFILE_BK
 		echo "47 2 * * *  /usr/ccsp/wifi/dailystats_log.sh" >> $CRONFILE
 		echo "30 * * * * /rdklogger/log_cpu_info_atom.sh" >> $CRONFILE_BK
+		echo "*/15 * * * * sh /usr/ccsp/wifi/radiohealth.sh" >> $CRONFILE_BK
 		echo "1 */12 * * *  /usr/ccsp/wifi/mesh_status.sh" >> $CRONFILE_BK
 		crontab $CRONFILE_BK -c $CRONPATH
 		rm -rf $CRONFILE_BK
