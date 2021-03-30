cde535fdecdf1666fe54ed45eb76db2d9781eaaf
Andre McCurdy <armccurdy@gmail.com>
2021-03-30 10:14:15 -0700

    RDKCOM-2270: RDKBDEV-528: add missing #!/bin/sh in script files
    cosa_start_arm.sh: add #!/bin/sh
    Currently the cosa_start.sh script is run via the shell interpreter
    explicitly (see service_cosa.sh). Adding the #!/bin/sh allows it to
    be started directly.
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>
    cosa_stop.sh: add #!/bin/sh
    Currently cosa_stop.sh is not used, but add #!/bin/sh for consistency
    with cosa_start.sh.
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>
    (cherry picked from commit 45de9d07c45fb986c74110145dcc0ff014468718)
    
    Reason for change: RDKBDEV-528: add missing #!/bin/sh in script files
    Test Procedure: Basic sanity + Check all the CCSP processes are up and running.
    Risks: Low.
    
    (cherry picked from commit 7fad5448e586b26ad9657c158834cb2eac35d585)
    Change-Id: Iddb63905838ab8f67c8c9be9ead6187a53408982

--- scripts/cosa_start_atom.sh
+++ scripts/cosa_start_atom.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 ##########################################################################
 # If not stated otherwise in this file or this component's Licenses.txt
 # file the following copyright and licenses apply:
